211b0a4fd484a970776f01850ce8f40275f11725
yxy <summerhh54@gmail.com>
2023-07-09 07:31:48 +0000

    Change function signature

--- riscv/src/MMult_4x4_12.h
+++ riscv/src/MMult_4x4_12.h
@@ -119,7 +119,7 @@ void InnerKernel( int m, int n, int k, float *a, int lda,
   }
 }
 
-void MY_MMult_4x4_13( int m, int n, int k, float *a, int lda, 
+void MY_MMult_4x4_12( int m, int n, int k, float *a, int lda, 
                                     float *b, int ldb,
                                     float *c, int ldc ) 
 {
