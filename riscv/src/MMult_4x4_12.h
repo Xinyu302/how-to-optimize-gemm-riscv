a0698a9ccc30f8093399a4869ef729dafb5dfb65
yxy <summerhh54@gmail.com>
2023-07-09 07:25:58 +0000

    rename riscv/src/MMult_4x4_13.h to riscv/src/MMult_4x4_12.h; same as https://github.com/tpoisonooo/how-to-optimize-gemm

--- riscv/src/MMult_4x4_12.h
+++ riscv/src/MMult_4x4_12.h
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <riscv_vector.h>
 
 /* Block sizes */
-#define mc 256 
+#define mc 256
 #define kc 128
 
 /* Create macros so that the matrices are stored in row-major order */
@@ -26,89 +27,75 @@ void PackMatrixB( int k, float *b, int ldb, float *b_to)
   }
 }
 
-#include <mmintrin.h>
-#include <xmmintrin.h>  // SSE
-#include <pmmintrin.h>  // SSE2
-#include <emmintrin.h>  // SSE3
-
-typedef union
+void PackMatrixA( int k, float *a, int lda, float * a_to)
 {
-  __m128 v;
-  float d[4];
-} v2df_t;
+  int i;
+  float
+    *a_0i_pntr = a,
+    *a_1i_pntr = a + lda,
+    *a_2i_pntr = a + (lda << 1),
+    *a_3i_pntr = a + (3 * lda);
 
+  for (i = 0; i < k; ++i) {
+    *a_to++ = *a_0i_pntr++;
+    *a_to++ = *a_1i_pntr++;
+    *a_to++ = *a_2i_pntr++;
+    *a_to++ = *a_3i_pntr++;
+  }
+  
+}
 
 void AddDot4x4( int k, float *a, int lda,  float *b, int ldb, float *c, int ldc )
 {
-  /* So, this routine computes a 4x4 block of matrix A
+  size_t vlmax = vsetvlmax_e32m1();
 
-           C( 0, 0 ), C( 0, 1 ), C( 0, 2 ), C( 0, 3 ).  
-           C( 1, 0 ), C( 1, 1 ), C( 1, 2 ), C( 1, 3 ).  
-           C( 2, 0 ), C( 2, 1 ), C( 2, 2 ), C( 2, 3 ).  
-           C( 3, 0 ), C( 3, 1 ), C( 3, 2 ), C( 3, 3 ).  
+  vfloat32m1_t c_p0_sum = vfmv_v_f_f32m1(0.0f, vlmax);
+  vfloat32m1_t c_p1_sum = vfmv_v_f_f32m1(0.0f, vlmax);
+  vfloat32m1_t c_p2_sum = vfmv_v_f_f32m1(0.0f, vlmax);
+  vfloat32m1_t c_p3_sum = vfmv_v_f_f32m1(0.0f, vlmax);
 
-     Notice that this routine is called with c = C( i, j ) in the
-     previous routine, so these are actually the elements 
-
-           C( i  , j ), C( i  , j+1 ), C( i  , j+2 ), C( i  , j+3 ) 
-           C( i+1, j ), C( i+1, j+1 ), C( i+1, j+2 ), C( i+1, j+3 ) 
-           C( i+2, j ), C( i+2, j+1 ), C( i+2, j+2 ), C( i+2, j+3 ) 
-           C( i+3, j ), C( i+3, j+1 ), C( i+3, j+2 ), C( i+3, j+3 ) 
-	  
-     in the original matrix C 
-
-     In this version, we use registers for elements in the current row
-     of B as well */
-
-  float 
-    /* Point to the current elements in the four rows of A */
-    *a_0p_pntr, *a_1p_pntr, *a_2p_pntr, *a_3p_pntr;
-
-  a_0p_pntr = &A(0, 0);
-  a_1p_pntr = &A(1, 0);
-  a_2p_pntr = &A(2, 0);
-  a_3p_pntr = &A(3, 0);
-
-  v2df_t c_p0_sum;
-  v2df_t c_p1_sum;
-  v2df_t c_p2_sum;
-  v2df_t c_p3_sum;
-  v2df_t  a_0p_reg, a_1p_reg, a_2p_reg, a_3p_reg, b_reg;
-
-  c_p0_sum.v = _mm_setzero_ps();
-  c_p1_sum.v = _mm_setzero_ps();
-  c_p2_sum.v = _mm_setzero_ps();
-  c_p3_sum.v = _mm_setzero_ps();
-  a_0p_reg.v = _mm_setzero_ps();
-  a_1p_reg.v = _mm_setzero_ps();
-  a_2p_reg.v = _mm_setzero_ps();
-  a_3p_reg.v = _mm_setzero_ps();
-
+  register float
+  a_0p_reg,
+  a_1p_reg,   
+  a_2p_reg,
+  a_3p_reg;
   for (int p = 0; p < k; ++p) {
-    b_reg.v = _mm_load_ps((float *)&B(p, 0));
+    vfloat32m1_t b_reg = vle32_v_f32m1(b, vlmax);
+    b += 4;
 
-    a_0p_reg.v = _mm_set_ps1(*a_0p_pntr++);
-    a_1p_reg.v = _mm_set_ps1(*a_1p_pntr++);
-    a_2p_reg.v = _mm_set_ps1(*a_2p_pntr++);
-    a_3p_reg.v = _mm_set_ps1(*a_3p_pntr++);
+    a_0p_reg = a[0];
+    a_1p_reg = a[1];
+    a_2p_reg = a[2];
+    a_3p_reg = a[3];
+    a += 4;
 
-    c_p0_sum.v += b_reg.v * a_0p_reg.v;
-    c_p1_sum.v += b_reg.v * a_1p_reg.v;
-    c_p2_sum.v += b_reg.v * a_2p_reg.v;
-    c_p3_sum.v += b_reg.v * a_3p_reg.v;
+    c_p0_sum = vfmacc_vf_f32m1(c_p0_sum, a_0p_reg, b_reg, vlmax);
+    c_p1_sum = vfmacc_vf_f32m1(c_p1_sum, a_1p_reg, b_reg, vlmax);
+    c_p2_sum = vfmacc_vf_f32m1(c_p2_sum, a_2p_reg, b_reg, vlmax);
+    c_p3_sum = vfmacc_vf_f32m1(c_p3_sum, a_3p_reg, b_reg, vlmax);
+    // 累加
   }
 
-  C(0, 0) += c_p0_sum.d[0]; C(0, 1) += c_p0_sum.d[1];
-  C(0, 2) += c_p0_sum.d[2]; C(0, 3) += c_p0_sum.d[3];
+  float *c_pntr = 0;
+  c_pntr = &C(0, 0);
+  vfloat32m1_t c_reg = vle32_v_f32m1(c_pntr, vlmax);
+  c_reg = vfadd_vv_f32m1(c_reg, c_p0_sum, vlmax);
+  vse32_v_f32m1(c_pntr, c_reg, vlmax);
 
-  C(1, 0) += c_p1_sum.d[0]; C(1, 1) += c_p1_sum.d[1];
-  C(1, 2) += c_p1_sum.d[2]; C(1, 3) += c_p1_sum.d[3];
+  c_pntr = &C(1, 0);
+  c_reg = vle32_v_f32m1(c_pntr, vlmax);
+  c_reg = vfadd_vv_f32m1(c_reg, c_p1_sum, vlmax);
+  vse32_v_f32m1(c_pntr, c_reg, vlmax);
 
-  C(2, 0) += c_p2_sum.d[0]; C(2, 1) += c_p2_sum.d[1];
-  C(2, 2) += c_p2_sum.d[2]; C(2, 3) += c_p2_sum.d[3];
+  c_pntr = &C(2, 0);
+  c_reg = vle32_v_f32m1(c_pntr, vlmax);
+  c_reg = vfadd_vv_f32m1(c_reg, c_p2_sum, vlmax);
+  vse32_v_f32m1(c_pntr, c_reg, vlmax);
 
-  C(3, 0) += c_p3_sum.d[0]; C(3, 1) += c_p3_sum.d[1];
-  C(3, 2) += c_p3_sum.d[2]; C(3, 3) += c_p3_sum.d[3];
+  c_pntr = &C(3, 0);
+  c_reg = vle32_v_f32m1(c_pntr, vlmax);
+  c_reg = vfadd_vv_f32m1(c_reg, c_p3_sum, vlmax);
+  vse32_v_f32m1(c_pntr, c_reg, vlmax);
 }
 
 void InnerKernel( int m, int n, int k, float *a, int lda, 
@@ -116,20 +103,23 @@ void InnerKernel( int m, int n, int k, float *a, int lda,
                                        float *c, int ldc )
 {
   int i, j;
+  float packedA[m * k];
   float packedB[k * n];
 
   for ( j=0; j<n; j+=4 ){        /* Loop over the columns of C, unrolled by 4 */
-    PackMatrixB(k, &B(0, j), ldb, &packedB[ j * k]);
+    PackMatrixB(k, &B(0, j), ldb, packedB + j * k);
     for ( i=0; i<m; i+=4 ){        /* Loop over the rows of C */
       /* Update C( i,j ), C( i,j+1 ), C( i,j+2 ), and C( i,j+3 ) in
 	 one routine (four inner products) */
-
-      AddDot4x4( k, &A( i,0 ), lda, &packedB[j * k], 4, &C( i,j ), ldc );
+      if (0 == j) {
+        PackMatrixA(k, &A(i, 0), lda, packedA + i * k);
+      }
+      AddDot4x4( k, packedA + i * k, k, packedB + j * k, 4, &C( i,j ), ldc );
     }
   }
 }
 
-void MY_MMult_4x4_12( int m, int n, int k, float *a, int lda, 
+void MY_MMult_4x4_13( int m, int n, int k, float *a, int lda, 
                                     float *b, int ldb,
                                     float *c, int ldc ) 
 {
